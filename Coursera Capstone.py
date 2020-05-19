4ebdee181587c2572a38670602b5dde4f2b399f7
sdixit108 <65564442+sdixit108@users.noreply.github.com>
2020-05-20 01:53:25 +0530

    Add files via upload

---
+++ Coursera Capstone.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera Capstone
+
+# In[3]:
+
+
+#1. Write some markdown to explain that this notebook will be use for the capstone.
+
+
+# This notebook will be mainly used for the capstone project for IBM Data Science Capstone course on Coursera.
+
+# In[5]:
+
+
+#2. Import the pandas library as pd.
+import pandas as pd
+
+
+# In[6]:
+
+
+#3. Import the Numpy Library asnp.
+import numpy as np
+
+
+# In[7]:
+
+
+#4. Print the following statement: Hello Capstone Project Coursera!
+print("Hello Capstone Project Coursera!")
+
